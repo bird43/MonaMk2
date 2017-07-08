a4f0a8dbfc86b83dd43770144295077f297f9e66
bird43 <Ben@MacBook-Pro-3.local>
2017-07-08 14:14:52 +0100

    Addition of examples
    A brief usage of all functions

--- examples/MonaMK2Test/MonaMK2Test.ino
+++ examples/MonaMK2Test/MonaMK2Test.ino
@@ -5,7 +5,7 @@ MONA mona;
 void setup(){
   
   unsigned char DIR = 0; // either 0 or 1 for forwards and backwards
-  unsigned char PWM = 50; // between 0 and 255
+  unsigned char PWM = 0; // between 0 and 255
   mona.InitMotors();
   mona.DriveRight(PWM, DIR);
   mona.DriveLeft(PWM, DIR);
@@ -17,8 +17,12 @@ void loop(){
   
   long RightEncoder = 0;
   long LeftEncoder = 0;
+  long LeftTime = 0;
+  long RightTime = 0;
+  long OdomLeft = 0;
   int data[5];
   int battery;
+  float LeftVelocity, RightVelocity;
   
   while(1){
       RightEncoder = mona.GetRightEncoder();
@@ -27,20 +31,39 @@ void loop(){
       mona.ReturnIRSensorData(data); // this will update the data array
       mona.DisableIRSensors(); // not really required, illustrated if required
       battery = mona.GetBattery();
-      
+      LeftTime = mona.LeftTimePeriod();
+      RightTime = mona.RightTimePeriod();
+      LeftVelocity = mona.LeftVelocity();
+      RightVelocity = mona.RightVelocity();
+      OdomLeft = mona.GetLeftOdom();
+      Serial.println(OdomLeft);
+      /*
       Serial.print("IR sensor data = ");
       for (int i = 0; i < 5; i++){
         Serial.print(data[i]);
         Serial.print("    ");
       }
+      
       Serial.println("  ");
       Serial.print("Battery level = ");
       Serial.println(battery);
       Serial.println("  ");
+      
       Serial.print("Right Encoder = ");
       Serial.println(RightEncoder);
       Serial.print("Left Encoder = ");
       Serial.println(LeftEncoder);
+      
+      Serial.print("Right time in microseconds  ");
+      Serial.println(RightTime);
+      Serial.print("Leftt time in microseconds  ");
+      Serial.println(LeftTime);
+
+      Serial.print("Right velocity  ");
+      Serial.print((float)(RightVelocity),4);
+      Serial.print("Leftt velocity  ");
+      Serial.println((float)(LeftVelocity),4);
+     */ 
       delay(1000);
   }
 }
